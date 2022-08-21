c5dc515ba3bb6294b0f11929dfb71e7ee457f411
unknown <skotkar@outlook.com>
2022-08-21 04:53:44 -0700

    Updated text message in method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,8 @@
 package com.example;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("This is an test message!");
     }
 }
